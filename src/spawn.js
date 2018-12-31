5e90a12c85f6100e24ecfb63e4551068bafd7b7b
Felipe Castillo <fcastillo.ec@gmail.com>
2018-12-31 14:30:52 -0800

    Move common functionality to electron-installer-common

--- src/spawn.js
+++ src/spawn.js
@@ -1,19 +1,11 @@
 'use strict'
 
-const spawn = require('cross-spawn-promise')
+const { spawn } = require('electron-installer-common')
 
 module.exports = function (cmd, args, logger) {
   if (process.platform !== 'win32' && cmd !== './src/cli.js') {
     args.unshift(cmd)
     cmd = 'mono'
   }
-
-  if (logger) logger(`Executing command: ${cmd} ${args.join(' ')}`)
-
-  return spawn(cmd, args)
-    .then(stdout => stdout.toString())
-    .catch(err => {
-      const stderr = err.stderr ? err.stderr.toString() : ''
-      throw new Error(`Error executing command (${err.message || err}):\n${cmd} ${args.join(' ')}\n${stderr}`)
-    })
+  return spawn(cmd, args, logger, null)
 }
