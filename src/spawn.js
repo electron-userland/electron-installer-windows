5d174cec5dd30b4e9ea3ee1d6e16a575b54080e3
Mark Lee <electronjs@lazymalevolence.com>
2019-01-12 12:53:43 -0800

    More user-friendly spawn implementation

--- src/spawn.js
+++ src/spawn.js
@@ -1,11 +1,33 @@
 'use strict'
 
 const { spawn } = require('electron-installer-common')
+const which = require('which')
+
+function updateExecutableMissingException (err, updateError) {
+  if (updateError && err.code === 'ENOENT' && err.syscall === 'spawn mono') {
+    let installer
+    let pkg
+
+    if (process.platform === 'darwin') {
+      installer = 'brew'
+      pkg = 'mono'
+    } else if (which.sync('dnf', { nothrow: true })) {
+      installer = 'dnf'
+      pkg = 'mono-core'
+    } else { // assume apt-based Linux distro
+      installer = 'apt'
+      pkg = 'mono-runtime'
+    }
+
+    err.message = `Your system is missing the ${pkg} package. Try, e.g. '${installer} install ${pkg}'`
+    }
+  }
+}
 
 module.exports = function (cmd, args, logger) {
-  if (process.platform !== 'win32' && cmd !== './src/cli.js') {
+  if (process.platform !== 'win32') {
     args.unshift(cmd)
     cmd = 'mono'
   }
-  return spawn(cmd, args, logger, null)
+  return spawn(cmd, args, logger, updateExecutableMissingException)
 }
