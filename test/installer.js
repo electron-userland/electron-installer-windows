d469cfc2ebecfdb365a95836e75822039b5815bc
Felipe Castillo <fcastillo.ec@gmail.com>
2019-03-18 13:28:45 -0700

    Test for no description and no authors

--- test/installer.js
+++ test/installer.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const describeInstaller = require('./helpers/describe_installer')
+const { describeInstallerWithException } = require('./helpers/describe_installer')
 const Server = require('./helpers/server')
 
 describe('module', function () {
@@ -17,6 +18,22 @@ describe('module', function () {
       'Utility'
     ]
   })
+
+  describeInstallerWithException(
+    'with no description or productDescription provided',
+    {
+      src: 'test/fixtures/app-without-description-or-product-description/'
+    },
+    /^No Description or ProductDescription provided/
+  )
+
+  describeInstallerWithException(
+    'with no authors provided',
+    {
+      src: 'test/fixtures/app-without-authors/'
+    },
+    /^No Authors provided/
+  )
 
   // Signing only works on Win32.
   if (process.platform === 'win32') {
